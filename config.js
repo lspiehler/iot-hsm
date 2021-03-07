0505f1375b6c31d3375fda602c1c65b9c2186287
Lyas Spiehler <lspiehler@gmail.com>
2021-03-07 16:41:38 +0000

    allow passing pins as env vars

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-	PLATFORMFQDN: process.env.PLATFORMFQDN || 'pkiaas.io'
+	PLATFORMFQDN: process.env.PLATFORMFQDN || 'pkiaas.io',
+	USERPIN: process.env.USERPIN || '123456',
+	SOPIN: process.env.SOPIN || '010203040506070801020304050607080102030405060708'
 }
