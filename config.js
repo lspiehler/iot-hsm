cf3c8aab082864bdb2acf0b3fb0ae19c32b67360
Lyas Spiehler <lspiehler@gmail.com>
2021-03-19 18:43:20 +0000

    use lib variable

--- config.js
+++ config.js
@@ -3,5 +3,6 @@ require('dotenv').config();
 module.exports = {
 	PLATFORMFQDN: process.env.PLATFORMFQDN || 'pkiaas.io',
 	USERPIN: process.env.USERPIN || '123456',
-	SOPIN: process.env.SOPIN || '010203040506070801020304050607080102030405060708'
+	SOPIN: process.env.SOPIN || '010203040506070801020304050607080102030405060708',
+	LIB: process.env.LIB || '/usr/lib'
 }
