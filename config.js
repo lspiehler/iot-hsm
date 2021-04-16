c6e116aa3bd6c47de893b6e25e5b15ed148d185b
Lyas Spiehler <lspiehler@gmail.com>
2021-04-16 11:35:15 +0000

    add support for changing port and listening ip

--- config.js
+++ config.js
@@ -4,5 +4,7 @@ module.exports = {
 	PLATFORMFQDN: process.env.PLATFORMFQDN || 'pkiaas.io',
 	USERPIN: process.env.USERPIN || '123456',
 	SOPIN: process.env.SOPIN || '010203040506070801020304050607080102030405060708',
-	LIB: process.env.LIB || '/usr/lib'
+	LIB: process.env.LIB || '/usr/lib',
+	LISTENIP: process.env.LISTENIP || '0.0.0.0',
+	PORT: process.env.PORT || 3000
 }
